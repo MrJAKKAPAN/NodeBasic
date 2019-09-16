fb2d4e9842de8b4f2b508f720e8067f46839c65b
Jakkapan Sitthikan <44072352+MrJAKKAPAN@users.noreply.github.com>
2019-09-17 00:48:23 +0700

    3 commit

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/* express-validator */
+const { check, validationResult } = require('express-validator');
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render("blog");
@@ -10,10 +14,19 @@ router.get('/add', function(req, res, next) {
   res.render("addblog");
 });
 
-router.post('/add', function(req, res, next) {
-  console.log(req.body.name);
-  console.log(req.body.description);
-  console.log(req.body.author);
+router.post('/add',[ 
+  check("name","กรุณาป้อนชื่อบทความ").not().isEmpty(), 
+  check("description","กรุณาป้อนรายละเอียด").not().isEmpty(), 
+  check("author","กรุณาป้อนชื่อผู้แต่ง").not().isEmpty() 
+], function(req, res, next) {
+  const errors = validationResult(req);
+  var  errors=result.errors;
+  if (!errors.isEmpty()) {
+    res.rander('addblog',{errors:errors});
+  }else{
+    
+  }
+
 });
 
 module.exports = router;
