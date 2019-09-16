83caf95c1233e20a7e4f62478d90daa9ab4daac5
Jakkapan Sitthikan <44072352+MrJAKKAPAN@users.noreply.github.com>
2019-09-17 00:15:41 +0700

    add first commit

---
+++ routes/blog.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render("blog");
+});
+
+router.get('/add', function(req, res, next) {
+  res.render("addblog");
+});
+
+router.post('/add', function(req, res, next) {
+  console.log(req.body.name);
+  console.log(req.body.description);
+  console.log(req.body.author);
+});
+
+module.exports = router;
+ 
